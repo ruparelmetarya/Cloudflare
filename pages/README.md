e1efc6a9176f72d0725ecddf7217f7b4054747d1
Metarya Ruparel <ruparel.metarya1988@gmail.com>
2021-11-03 17:13:24 -0400

    Added pages

---
+++ pages/README.md
@@ -0,0 +1,17 @@
+# PAGES
+
+This contains the code to render the posts using Pages.
+
+You can see posts created. 
+
+NOTE: The like button API is implemented. However, when I call it on
+button click it throws an error. Hence, removed the API call for the
+submission purposes. I could have cracked it if I had more time on my hand.
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
---
+++ pages/src/components/posts.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "@reach/router";
+
+const Posts = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const resp = await fetch(
+                "https://worker.metarya.workers.dev/post"
+            );
+            const postsResp = await resp.json();
+            setPosts(postsResp);
+        };
+
+        getPosts();
+    }, []);
+    const divStyle = {
+        margin: 'auto',
+        marginBottom: '10px',
+        width: '50%',
+        border: '4px solid #f2f2f2',
+        borderRadius: '15px',
+        padding: '10px',
+        backgroundColor: '#FFFFFF',
+        WebkitTransition: 'all', // note the capital 'W' here
+        msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+    };
+    const bodStyle = {
+        backgroundColor: '#f2f2f2',
+        WebkitTransition: 'all', // note the capital 'W' here
+        msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+    };
+    let likePostAPI = async (post) => {
+
+    }
+    return (
+        <html>
+        <body style={bodStyle}>
+        <div>
+            <h1 style={{textAlign:'center'}}>Posts</h1>
+            {posts.map((post) => (
+                <div style={divStyle} key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.content}</p>
+                    <p>
+                        <em>Published {new Date(post.published_at).toLocaleString()}</em>
+                    </p>
+                    <div style={{width: '30%'}}>
+                        <div style={{width: "30%", height: "50px", float: "left"}}>
+                            👍 {post.likes}
+                        </div>
+                        <div style={{marginLeft: "0%",height: "50px", float: "left"}}>
+                            😂 {post.laughs}
+                        </div>
+                        <div style={{marginLeft: "60%",height: "50px"}}>
+                            ❤️ {post.loves}
+                        </div>
+                    </div>
+                    <div style={{width: '50%'}}>
+                        <div style={{width: "20%", height: "50px", float: "left"}}>
+                            <button onClick={{post}.likes+=1}> 👍️ Like </button>
+                        </div>
+                        <div style={{marginLeft: "0%",height: "50px", float: "left"}}>
+                            <button> 😂 Laugh </button>
+                        </div>
+                        <div style={{marginLeft: "45%",height: "50px"}}>
+                            <button> ❤️ Love </button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+        </body>
+        </html>
+    );
+};
+
+export default Posts;
